66f87eda0e33b65c5019fa77684edf59d9eaafeb
bardiasalekani <saba24pi@student.ju.se>
2025-03-06 17:03:01 +0100

    adding slider to the home page

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let slides = document.querySelectorAll(".slide");
+let current = 0;
+
+function changeSlide() {
+    slides[current].classList.remove("active");
+    current = current +1;
+    if(current >= slides.length) {
+        current = 0;
+    }
+
+    slides[current].classList.add("active");
+}
+
+setInterval(changeSlide, 3000);
