006a9622c9d7fee79a84514fb17f49a2d54f308c
ArshamDavariShaslamzari <arshdav1376@gmail.com>
2025-03-10 20:00:58 +0100

    adding other js part for card

--- js/cart.js
+++ js/cart.js
@@ -9,10 +9,24 @@ function addToCart(name, image, price) {
 function showCart() {
   if (cartItem) {
     cartItem.innerHTML = "";
-    cart.array.forEach(item,index)=> {
-     let div = document.createElement("div")
-    }
-     
+    cart.array.forEach((item, index) => {
+      let div = document.createElement("div");
+      div.className = "cartItem";
+      div.innerHTML = `
+     <img src="${item.image}" alt="${item.name}">
+     <span>${item.name} - ${item.price}</span>
+     <button onclick="removeFromCart(${index})">Remove</button>`;
+      cardItem.appendChild(div);
     });
   }
+}
+
+function removeFromCart(index) {
+  cart.splice(index, 1);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  showCart();
+}
+
+if (document.getElementById("cartItems")) {
+  showCart();
 }
